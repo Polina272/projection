3f129843757737159eccf9356e62df390a46b664
Polina272 <56951602+Polina272@users.noreply.github.com>
2021-05-17 00:58:03 +0300

    Update Create_Server.js

--- Create_Server.js
+++ Create_Server.js
@@ -7,8 +7,8 @@ http.createServer(function(request, response){
     if(request.url === "/home" || request.url === "/"){
         response.write("<h2>New Server</h2>");
     }
-    else if(request.url == "/is9308"){
-        response.write("<h2>Киричук Ірина 2 курс ІС-93</h2>");
+    else if(request.url == "/is9325"){
+        response.write("<h2>Сіденко Поліна 2 курс ІС-93</h2>");
     }
     else{
         response.write("<h2>Not found</h2>");
